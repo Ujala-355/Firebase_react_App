2e51c414442179293caa38132155755ce5f1a998
Ujala <ujala21@navgurukul.com>
2023-11-17 17:56:15 +0530

    read firestore

--- src/firestore.js
+++ src/firestore.js
@@ -1,35 +1,4 @@
-// import {getFirestore, collection, addDoc} from "firebase/firestore";
-// import {app} from "./firebase";
-
-// const firestore=getFirestore(app);
-// const FireStore=()=>{
-//     const writeData=async()=>{
-//         const result=await addDoc(collection(firestore,'cities'),{
-//             name:"kanpur",
-//             pinCode:208017,
-//             lat:145,
-//             long:555,  
-//         })
-//         console.log('Result',result)
-//     }
-//     const makeSubCollection=async()=>{
-//         await  addDoc(collection,(firestore,'cities/n3fgW9lIy3YQQ5BM7QF0/places'),{
-//             name:'this a place',
-//             desc:"Awsm Desc",
-//             data:Date.now(),
-//         })
-//     }
-//     return(
-//         <>
-//             <h1>FireStore</h1>
-//             <button onClick={writeData}>Put Data</button>
-//             <button onClick={makeSubCollection}>put SubData</button>
-//         </>
-//     )
-// }
-// export default FireStore;
-
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getFirestore, collection, addDoc, doc, getDoc } from "firebase/firestore";
 import { app } from "./firebase";
 
 const firestore = getFirestore(app);
@@ -52,11 +21,23 @@ const FireStore = () => {
     });
   };
 
+  const getDocument = async () => {
+    const ref = doc(firestore, "cities", "n3fgW9lIy3YQQ5BM7QF0");
+    const documentSnapshot = await getDoc(ref);
+    
+    if (documentSnapshot.exists()) {
+      console.log('Document data:', documentSnapshot.data());
+    } else {
+      console.log('Document does not exist');
+    }
+  };
+
   return (
     <>
       <h1>FireStore</h1>
       <button onClick={writeData}>Put Data</button>
       <button onClick={makeSubCollection}>Put SubData</button>
+      <button onClick={getDocument}>Get Document</button>
     </>
   );
 };
